cbf5abf6c916efea1e5137d22315e9cee67760d5
SurmaSb <surmasb51@gmail.com>
2018-05-29 19:46:51 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
+import Header from './Header';
+import Description from './Description';
+import './App.css';
 
-interface Props {
-  name?: string;
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Header name="REACT" />
+        </header>
+        <Description countBy={3} />
+      </div>
+    );
+  }
 }
 
-const Header: React.SFC<Props> = (props: Props) => (
-  <h1>
-    Hello, {props.name}! Welcome to React and TypeScript.
-  </h1>
-);
-
-Header.defaultProps = {
-  name: 'world',
-};
-
-export default Header;
+export default App;
 
