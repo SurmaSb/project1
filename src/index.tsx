700d5d8e8fa0c47e83e0404e8eae72bf7550cdf8
SurmaSb <surmasb51@gmail.com>
2018-05-29 20:59:00 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import 'resources/main.less';
+import 'resources/var/lib/jenkins/workspace/project1_project1_master-NKIZ7GAI62VYEDYVGYRYJWMHZNDS43UP4QFFX6IAL6A2WNYIZHAQ/src/index.tsx';
 
 ReactDOM.render(
   <App />,
