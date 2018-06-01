e98b788f24a93cd19fda564d783a1801e773b16f
SurmaSb <surmasb51@gmail.com>
2018-06-01 14:04:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import * as React from 'react';
+import Header from './Header';
+import Description from './Description';
 import './App.css';
 
-import logo from './logo.svg';
+const logo = require('./logo.svg');
 
 class App extends React.Component {
-  public render() {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Header name="REACT" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Description countBy={3} />
       </div>
     );
   }
 }
-
-export default App;
