fec289ce6bc53aac7e6b90166d5201f36e33d3f7
SurmaSb <surmasb51@gmail.com>
2018-06-01 16:12:14 +0700

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
