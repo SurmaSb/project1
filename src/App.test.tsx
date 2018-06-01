7fb00b3b8ff4611be811a75df0dee4110f9be577
SurmaSb <surmasb51@gmail.com>
2018-06-01 16:17:25 +0700

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,8 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.render((
+ ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>
