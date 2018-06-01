af2da1b1b49d1494016a7be5a3df462f4b00424b
SurmaSb <surmasb51@gmail.com>
2018-06-01 14:39:20 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,8 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-      <ul>
-        <li>
-           <a hreff="/">Главная</a>
-         </li>
-      </ul>
       
-      </div>
+       </div>
     );
   }
 }
