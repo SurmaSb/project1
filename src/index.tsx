403a056bdff1072baf1b6ddff26d5e052ac7cbb2
SurmaSb <surmasb51@gmail.com>
2018-06-05 00:17:26 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import createBrowserHistory from 'history/createBrowserHistory';
+
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
-import {Provider} from 'react-redux';
-import {Route, Router} from 'react-router';
+
+
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
