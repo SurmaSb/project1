0d395af433d30a4bb43c68f724cacfc61c851d31
SurmaSb <surmasb51@gmail.com>
2018-06-05 00:05:04 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-import App from './App';
 import createBrowserHistory from 'history/createBrowserHistory';
-import registerServiceWorker from './registerServiceWorker';
-
-import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import  {Provider } from 'react-redux';
-import  {Router, Route}  from 'react-router';
+import * as ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+import {Route, Router} from 'react-router';
+import App from './App';
 import './index.css';
-
+import registerServiceWorker from './registerServiceWorker';
 
 
 
@@ -18,16 +16,8 @@ const browserHistory = createBrowserHistory();
 
 
 
-ReactDOM.render( (
-        <Provider>
-
-            <Router history={browserHistory}>
-
-                <Route path="/" component={App}/>
-            </Router>
-        </Provider>),
-
-
+ReactDOM.render( 
+      
     document.getElementById('root') as HTMLElement
 );
 
