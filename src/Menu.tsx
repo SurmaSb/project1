f63eb97fd68128353a8f2e8c3f0bf0d5906215fd
SurmaSb <surmasb@gmeil.com>
2018-09-04 19:52:41 +0800

    r

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 class Menu extends React.Component {
-   public render() {
+    public render() {
         return (
             <div>
                 <Link to="/">Tracks</Link>
